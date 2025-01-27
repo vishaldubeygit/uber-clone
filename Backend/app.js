ad4565a7e701c434cd5245380044ebcf7d9b48cd
Your Name <youremail@example.com>
2025-01-27 19:20:05 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const dotenv=require('dotenv');
+dotenv.config();
+const cors=require('cors');
+const app=express();
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('hello jee');
+
+});
+
+
+module.expoorts=app;
